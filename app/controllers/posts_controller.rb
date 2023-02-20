9f5573affccb7c3b836151ab1f2998d124d52a93
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-21 00:02:37 +0900

    アソシエーションとバリデーションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 
   def index
+    @post = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+  has_many_attached :images
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :article
+    validates :place
+    validates :user_id
+  end
+
+
 end
