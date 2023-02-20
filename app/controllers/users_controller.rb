870dbd25340885b6bfee62791349acb0f4a7dc70
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-20 15:48:53 +0900

    ユーザー詳細・編集画面仮作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = User.find(params[:id])
+    #@nickname = current_user.nickname
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
 
-
+  def user_params
+    params.require(:user).permit(:nickname, :last_name, :first_name, :email, :profile_image)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,6 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
 
+
+
 end
